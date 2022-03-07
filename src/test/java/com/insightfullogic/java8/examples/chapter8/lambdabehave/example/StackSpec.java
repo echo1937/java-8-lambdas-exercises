b62b958f7c6a27ffa232031068b9f88c3b38d730
echo1937 <1qaz@WSX3edc>
2022-03-07 16:53:07 +0800

    第一遍over

--- src/test/java/com/insightfullogic/java8/examples/chapter8/lambdabehave/example/StackSpec.java
+++ src/test/java/com/insightfullogic/java8/examples/chapter8/lambdabehave/example/StackSpec.java
@@ -1,34 +1,59 @@
 package com.insightfullogic.java8.examples.chapter8.lambdabehave.example;
 
+import com.insightfullogic.java8.examples.chapter8.lambdabehave.Description;
+import com.insightfullogic.java8.examples.chapter8.lambdabehave.Specification;
+import com.insightfullogic.java8.examples.chapter8.lambdabehave.Suite;
+import com.insightfullogic.java8.examples.chapter8.lambdabehave.expectations.Expect;
+
 import java.util.Stack;
 
 import static com.insightfullogic.java8.examples.chapter8.lambdabehave.Lets.describe;
 
 // BEGIN StackSpec
-public class StackSpec {{
+public class StackSpec {
+    {
 
-    describe("a stack", it -> {
+        describe("a stack",
+                new Suite() {
+                    @Override
+                    public void specifySuite(Description description) {
+                        // 1.
+                        description.should("be empty when created",
+                                new Specification() {
+                                    @Override
+                                    public void specifyBehaviour(Expect expect) {
+                                        expect.that(new Stack()).isEmpty();
+                                    }
+                                });
 
-        it.should("be empty when created", expect -> {
-            expect.that(new Stack()).isEmpty();
-        });
+                        // 2.
+                        description.should("push new elements onto the top of the stack", new Specification() {
+                            @Override
+                            public void specifyBehaviour(Expect expect) {
+                                Stack<Integer> stack = new Stack<>();
+                                stack.push(1);
 
-        it.should("push new elements onto the top of the stack", expect -> {
-            Stack<Integer> stack = new Stack<>();
-            stack.push(1);
+                                expect.that(stack.get(0)).isEqualTo(1);
+                            }
+                        });
 
-            expect.that(stack.get(0)).isEqualTo(1);
-        });
+                        // 3.
+                        description.should("pop the last element pushed onto the stack", new Specification() {
+                            @Override
+                            public void specifyBehaviour(Expect expect) {
+                                Stack<Integer> stack = new Stack<>();
+                                stack.push(2);
+                                stack.push(1);
 
-        it.should("pop the last element pushed onto the stack", expect -> {
-            Stack<Integer> stack = new Stack<>();
-            stack.push(2);
-            stack.push(1);
+                                expect.that(stack.pop()).isEqualTo(2);
+                            }
+                        });
 
-            expect.that(stack.pop()).isEqualTo(2);
-        });
-        
-    });
+                    }
 
-}}
+                }
+        );
+
+    }
+}
 // END StackSpec
